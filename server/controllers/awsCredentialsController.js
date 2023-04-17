031b2fc1165f68a92c89e6879b93a2ae1219a6e6
Andrii <karpuxa@gmail.com>
2023-04-17 14:25:23 -0400

    added controller to get temp AWS credentials and updated db.config to use AWS SDK v3

---
+++ server/controllers/awsCredentialsController.js
@@ -0,0 +1,38 @@
+const { AssumeRoleCommand } = require('@aws-sdk/client-sts');
+const { stsClient } = require('../db.config.js');
+
+const awsCredentialsController = {};
+
+awsCredentialsController.getCredentials = async (req, res, next) => {
+  const { userRoleArn } = req.body;
+
+  if (!userRoleArn) {
+    return res.status(400).json({ message: 'User Role ARN is required.' });
+  }
+
+  const assumeRoleParams = {
+    RoleArn: userRoleArn,
+    RoleSessionName: 'TestSession',
+  };
+
+  try {
+    const data = await stsClient.send(new AssumeRoleCommand(assumeRoleParams));
+    const temporaryCredentials = {
+      accessKeyId: data.Credentials.AccessKeyId,
+      secretAccessKey: data.Credentials.SecretAccessKey,
+      sessionToken: data.Credentials.SessionToken,
+    };
+
+    res.locals.awsCredentials = temporaryCredentials;
+    return next();
+  } catch (err) {
+    console.error('Error assuming role:', err);
+    let error = {
+      log: 'Express error handler caught awsCredentialsController.getCredentials',
+      message: { err: err },
+    };
+    return next(error);
+  }
+};
+
+module.exports = awsCredentialsController;
--- server/server.js
+++ server/server.js
@@ -1,66 +1,70 @@
-const express = require("express"),
-PORT = 3000,
-app = express();
-const userController = require( './controllers/userController');
-const redisController = require('./controllers/redisController')
+const express = require('express'),
+  PORT = 3000,
+  app = express();
+const userController = require('./controllers/userController');
+const redisController = require('./controllers/redisController');
+const awsCredentialsController = require('./controllers/awsCredentialsController');
+
 // import express from "express";
 // const PORT = 3000,
 // app = express();
 // import  { createUser } from './controllers/userController.js';
 // import { Module } from "module";
 
-
-
-
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-app.get("/api", (req,res) => {
-    let data = "hello";
-    res.status(200).json(data);
+app.get('/api', (req, res) => {
+  let data = 'hello';
+  res.status(200).json(data);
 });
 
-
-app.post("/createUser", userController.createUser, (req,res) => {
-    res.sendStatus(200);
-})
-
-app.get("/verifyUser", userController.verifyUser, (req,res) => {
-    //successful login
-    // res.redirect('homepage');
-    res.sendStatus(200);
+app.post('/createUser', userController.createUser, (req, res) => {
+  res.sendStatus(200);
 });
 
-app.post("/setLogs", redisController.setLogs, (req,res) => {
-    //successful login
-    // res.redirect('homepage');
-    res.sendStatus(200);
+app.get('/verifyUser', userController.verifyUser, (req, res) => {
+  //successful login
+  // res.redirect('homepage');
+  res.sendStatus(200);
 });
 
-app.get("/getLogs", redisController.getLogs, (req,res) => {
-    //successful login
-    // res.redirect('homepage');
-    res.status(200).json(res.locals.logs);
+app.post('/setLogs', redisController.setLogs, (req, res) => {
+  //successful login
+  // res.redirect('homepage');
+  res.sendStatus(200);
 });
-app.get("/getErrLogs", redisController.getErrLogs, (req,res) => {
-    //successful login
-    // res.redirect('homepage');
-    res.status(200).json(res.locals.logs);
+app.post(
+  '/getTempCredentials',
+  awsCredentialsController.getCredentials,
+  (req, res) => {
+    res.status(200).json(res.locals.awsCredentials);
+  }
+);
+
+app.get('/getLogs', redisController.getLogs, (req, res) => {
+  //successful login
+  // res.redirect('homepage');
+  res.status(200).json(res.locals.logs);
+});
+app.get('/getErrLogs', redisController.getErrLogs, (req, res) => {
+  //successful login
+  // res.redirect('homepage');
+  res.status(200).json(res.locals.logs);
 });
 
 //Route not found
-app.use((req,res) => res.sendStatus(404));
+app.use((req, res) => res.sendStatus(404));
 
 //Global error handler
 app.use((err, req, res, next) => {
-    const defaultErr = {
-        log:'Express error handler caught unkown middleware error',
-        status: 400,
-        message: { err : 'An error occurred'},
-    };
-    const errorObj = Object.assign({}, defaultErr, err);
-    return res.status(errorObj.status).json(errorObj.message);
+  const defaultErr = {
+    log: 'Express error handler caught unkown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
