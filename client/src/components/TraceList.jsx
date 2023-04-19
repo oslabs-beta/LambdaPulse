4aa0822f92272b5b6adadc48dc4ae663c182ddc5
Bryent <bryents@gmail.com>
2023-04-19 11:09:50 -0400

    changed clear redistraces to refreshdata

--- client/src/components/TraceList.jsx
+++ client/src/components/TraceList.jsx
@@ -8,20 +8,24 @@ function TraceList (props) {
         traces.push(<button onClick={() => props.setCurrentTrace(n)}>{props.traces[n].id}</button>)
     }
 
-    function clearRedis() {
+    function refreshData() {
+        //clears Traces table from redis
         fetch('/clearTraces',
-          {
+        {
             method: 'GET',
-          })
-          .then(result =>  {
+        })
+        .then(result =>  {
             console.log(result);
-            result})
+        })
+        //changes refresh which fires off useEffect(in dashboard.jsx) to fetch new data
+        props.setRefresh(!props.refresh)
+
     }
 
     return (
         <div>
             <p>TraceList</p>
-            <button onClick={()=>clearRedis()}>Clear TraceList</button>
+            <button onClick={()=>refreshData()}>Refresh Data</button>
             {traces}
         </div>
     )
